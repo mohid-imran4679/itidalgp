0620cde6164f449f42bccf5eec29ff5b80a58597
Global Perspectives <mohidimran4679@gmail.com>
2026-02-03 22:45:15 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+       const reportButton = document.getElementById('reportBtn');
+
+    reportButton.addEventListener('click', function() { 
+      
+        alert("Thank you for your report! The Itidal.gp team will review the evidence of illegal infrastructure immediately.");
+        
+        
+    });
+
+});
